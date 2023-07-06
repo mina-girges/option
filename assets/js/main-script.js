0264083bbd2983acd3c575d1cf473737c91e9f74
Mina Gerges <100945436+mina-girges@users.noreply.github.com>
2023-07-06 23:04:02 +0300

    Add files via upload

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,4 +1,4 @@
-let navbar = document.querySelector('.naqrah-header .navbar');
+let navbar = document.querySelector('.option-header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
